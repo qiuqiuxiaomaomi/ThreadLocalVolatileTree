3ee2ef1b253acdfcea8131170f17ba46ab376777
mingquan.yang@karakal.com.cn <mingquan.yang@karakal.com.cn>
2018-12-11 19:25:01 +0800

    ThreadLocal源码技术分析

--- README.md
+++ README.md
@@ -1,2 +1,46 @@
 # ThreadLocalVolatileTree
 ThreadLocal，Volatile源代码技术研究
+
+
+![](https://i.imgur.com/THhYN8u.png)
+
+<pre>
+      ThreadLocal的实现是这样的：每个Thread 维护一个 ThreadLocalMap 映射表，这个映射
+    表的 key 是 ThreadLocal实例本身，value 是真正需要存储的 Object。
+
+      也就是说 ThreadLocal 本身并不存储值，它只是作为一个 key 来让线程从 
+    ThreadLocalMap 获取 value。值得注意的是图中的虚线，表示 ThreadLocalMap 是使
+    用 ThreadLocal 的弱引用作为 Key 的，弱引用的对象在 GC 时会被回收。
+
+   
+    ThreadLocal为什么会内存泄漏：
+
+        ThreadLocalMap使用ThreadLocal的弱引用作为key，如果一个ThreadLocal没有外部强
+     引用来引用它，那么系统 GC 的时候，这个ThreadLocal势必会被回收，这样一来，
+     ThreadLocalMap中就会出现key为null的Entry，就没有办法访问这些key为null的Entry
+     的value，如果当前线程再迟迟不结束的话，这些key为null的Entry的value就会一直存在一
+     条强引用链：Thread Ref -> Thread -> ThreaLocalMap -> Entry -> value永远无法
+     回收，造成内存泄漏。
+
+     其实，ThreadLocalMap的设计中已经考虑到这种情况，也加上了一些防护措施：在
+     ThreadLocal的get(),set(),remove()的时候都会清除线程ThreadLocalMap里所有key
+     为null的value
+</pre>
+
+<pre>
+为什么使用弱引用
+
+      1）key 使用强引用：引用的ThreadLocal的对象被回收了，但是ThreadLocalMap还持
+      有ThreadLocal的强引用，如果没有手动删除，ThreadLocal不会被回收，导致Entry内存泄漏。
+      2）key 使用弱引用：引用的ThreadLocal的对象被回收了，由于ThreadLocalMap持
+      有ThreadLocal的弱引用，即使没有手动删除，ThreadLocal也会被回收。value在下一
+      次ThreadLocalMap调用set,get，remove的时候会被清除。
+
+      比较两种情况，我们可以发现：由于ThreadLocalMap的生命周期跟Thread一样长，如果都没
+      有手动删除对应key，都会导致内存泄漏，但是使用弱引用可以多一层保障：弱引
+      用ThreadLocal不会内存泄漏，对应的value在下一次ThreadLocalMap调用set,get,
+      remove的时候会被清除。
+
+      因此，ThreadLocal内存泄漏的根源是：由于ThreadLocalMap的生命周期跟Thread一样长，
+      如果没有手动删除对应key就会导致内存泄漏，而不是因为弱引用。
+</pre>
---
+++ ThreadLocal.java
@@ -0,0 +1,199 @@
+public class ThreadLocal<T> {
+    private final int threadLocalHashCode = nextHashCode();
+
+	通过getMap()获取每个子线程Thread持有自己的ThreadLocalMap实例, 因此它们是不存在并发竞争的。可以理解为每个线程有自己的变量副本
+    ThreadLocalMap getMap(Thread t) {
+        return t.threadLocals;
+    }
+
+    void createMap(Thread t, T firstValue) {
+        t.threadLocals = new ThreadLocalMap(this, firstValue);
+    }
+
+    public T get() {
+        Thread t = Thread.currentThread();
+        ThreadLocalMap map = getMap(t);
+        if (map != null) {
+            ThreadLocalMap.Entry e = map.getEntry(this);
+            if (e != null) {
+                @SuppressWarnings("unchecked")
+                T result = (T)e.value;
+                return result;
+            }
+        }
+        return setInitialValue();
+    }
+
+     public void remove() {
+         ThreadLocalMap m = getMap(Thread.currentThread());
+         if (m != null)
+             m.remove(this);
+     }
+
+	 ThreadLocalMap是一个定制的哈希映射，仅适用于维护线程本地值。ThreadLocalMap类是包私有的，
+	 允许在Thread类中声明字段。为了帮助处理非常大且长时间的使用，哈希表entry使用了对键的弱引用。、
+	 有助于GC回收
+    static class ThreadLocalMap {
+
+        static class Entry extends WeakReference<ThreadLocal<?>> {
+            /** The value associated with this ThreadLocal. */
+            Object value;
+
+            Entry(ThreadLocal<?> k, Object v) {
+                super(k);
+                value = v;
+            }
+        }
+
+        private static final int INITIAL_CAPACITY = 16;
+
+		存放数据
+        private Entry[] table;
+
+        private int size = 0;
+
+        private int threshold; // Default to 0
+
+        ThreadLocalMap(ThreadLocal<?> firstKey, Object firstValue) {
+            table = new Entry[INITIAL_CAPACITY];
+            int i = firstKey.threadLocalHashCode & (INITIAL_CAPACITY - 1);
+            table[i] = new Entry(firstKey, firstValue);
+            size = 1;
+            setThreshold(INITIAL_CAPACITY);
+        }
+
+        private ThreadLocalMap(ThreadLocalMap parentMap) {
+            Entry[] parentTable = parentMap.table;
+            int len = parentTable.length;
+            setThreshold(len);
+            table = new Entry[len];
+
+            for (int j = 0; j < len; j++) {
+                Entry e = parentTable[j];
+                if (e != null) {
+                    @SuppressWarnings("unchecked")
+                    ThreadLocal<Object> key = (ThreadLocal<Object>) e.get();
+                    if (key != null) {
+                        Object value = key.childValue(e.value);
+                        Entry c = new Entry(key, value);
+                        int h = key.threadLocalHashCode & (len - 1);
+                        while (table[h] != null)
+                            h = nextIndex(h, len);
+                        table[h] = c;
+                        size++;
+                    }
+                }
+            }
+        }
+
+        private void set(ThreadLocal<?> key, Object value) {
+            Entry[] tab = table;
+            int len = tab.length;
+            int i = key.threadLocalHashCode & (len-1);
+
+            for (Entry e = tab[i];
+                 e != null;
+                 e = tab[i = nextIndex(i, len)]) {
+                ThreadLocal<?> k = e.get();
+
+                if (k == key) {
+                    e.value = value;
+                    return;
+                }
+
+                if (k == null) {
+                    replaceStaleEntry(key, value, i);
+                    return;
+                }
+            }
+
+            tab[i] = new Entry(key, value);
+            int sz = ++size;
+            if (!cleanSomeSlots(i, sz) && sz >= threshold)
+                rehash();
+        }
+
+        /**
+         * Remove the entry for key.
+         */
+        private void remove(ThreadLocal<?> key) {
+            Entry[] tab = table;
+            int len = tab.length;
+            int i = key.threadLocalHashCode & (len-1);
+            for (Entry e = tab[i];
+                 e != null;
+                 e = tab[i = nextIndex(i, len)]) {
+                if (e.get() == key) {
+                    e.clear();
+                    expungeStaleEntry(i);
+                    return;
+                }
+            }
+        }
+
+        private void resize() {
+            Entry[] oldTab = table;
+            int oldLen = oldTab.length;
+            int newLen = oldLen * 2;
+            Entry[] newTab = new Entry[newLen];
+            int count = 0;
+
+            for (int j = 0; j < oldLen; ++j) {
+                Entry e = oldTab[j];
+                if (e != null) {
+                    ThreadLocal<?> k = e.get();
+                    if (k == null) {
+                        e.value = null; // Help the GC
+                    } else {
+                        int h = k.threadLocalHashCode & (newLen - 1);
+                        while (newTab[h] != null)
+                            h = nextIndex(h, newLen);
+                        newTab[h] = e;
+                        count++;
+                    }
+                }
+            }
+
+            setThreshold(newLen);
+            size = count;
+            table = newTab;
+        }
+		
+		//删除陈旧entry的核心方法
+		private int expungeStaleEntry(int staleSlot) {
+            Entry[] tab = table;
+            int len = tab.length;
+
+            // expunge entry at staleSlot
+            tab[staleSlot].value = null;
+            tab[staleSlot] = null;
+            size--;
+
+            // Rehash until we encounter null
+            Entry e;
+            int i;
+            for (i = nextIndex(staleSlot, len);
+                 (e = tab[i]) != null;
+                 i = nextIndex(i, len)) {
+                ThreadLocal<?> k = e.get();
+                if (k == null) {
+                    e.value = null;
+                    tab[i] = null;
+                    size--;
+                } else {
+                    int h = k.threadLocalHashCode & (len - 1);
+                    if (h != i) {
+                        tab[i] = null;
+
+                        // Unlike Knuth 6.4 Algorithm R, we must scan until
+                        // null because multiple entries could have been stale.
+                        while (tab[h] != null)
+                            h = nextIndex(h, len);
+                        tab[h] = e;
+                    }
+                }
+            }
+            return i;
+        }
+    }
+}
